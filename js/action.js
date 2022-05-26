6d43cf71359b86397538263bda90d4b30397c199
euihgois <iskand.agung@gmail.com>
2022-05-26 15:47:17 +0700

    tingal dom

--- js/action.js
+++ js/action.js
@@ -15,7 +15,7 @@ const db_ID = {//data ID & Password
 
 const db_skin = {//data untuk skin
     skin1: {
-        image: 'dummyImage',
+        image: 'imgs/1.png',// imgs/<nama file>.<extension>
         price: 40_000,
         stock: 10,
         rarity: 'common'
@@ -44,7 +44,7 @@ const db_skin = {//data untuk skin
         stock: 10,
         rarity: 'uncommon'
     },
-    skin5: {
+    skin6: {
         image: 'dummyImage',
         price: 900_000,
         stock: 10,
@@ -52,15 +52,11 @@ const db_skin = {//data untuk skin
     },
 }
 
-let user = 'orangBiasa'; //diambil dr getElement sign in, untuk dummy
-let userWallet = db_ID[user].wallet;//untuk display
-let tempWallet = userWallet;
-let collectedMoney = 0;
-
+let userInput;//diperlukan agar bisa akses wallet user di db_ID
+let userWallet = db_ID[userInput].wallet;//untuk display
+let tempWallet = userWallet;//temp untuk function reset
+let collectedMoney = 0;//state awal
 let userChart = [];//untuk display, setiap element berbentuk object {value: <nama skin>, cancelable: <true/false>}
-
-let itemClicked1 = 'skin2'; //diambil dr getElement di tombol item list apabila user mengklik tombol
-let itemClicked2 = 'skin3';
 let displayChange; //untuk display kembalian
 
 //function getMoney untuk mengambil user wallet sejumlah nominal di tombol
@@ -72,7 +68,7 @@ function getMoney(nominal) {//asumsi parameter bertipe Number, jika tidak perlu 
 }
 
 //function calculcation akan melakukan update untuk kedua data base ketika tombol, jika bisa ada status yang memberitahu bahwa uang kurang
-function calculation(skin) {//tergantung skinnya apa yang dipencet
+function calculation(skin) {//tergantung skinnya apa yang dipencet, jika tombol skin1, masukan 'skin1' pada function, maka calculation('skin')
     if (collectedMoney >= db_skin[skin].price) {
         collectedMoney -= db_skin[skin].price;//duit di vending berkurang
         db_skin[skin].stock--; //stock berkurang
@@ -119,6 +115,7 @@ function checkOut() {
     displayChange = userWallet; //untuk display kembalian
     userWallet += collectedMoney;
     userChart = [];
+    collectedMoney = 0;
 }
 
 //function filterByRarity untuk function groupChoice
@@ -132,7 +129,6 @@ function filterByRarity(rarity) {
     return result;
 }
 
-
 //function tambahan untuk function gacha, memilih secara random (memiliki chance yang sama karena pseudo-random nya bersifat uniform distribution dalam range tersebut)
 function groupChoice(group) {//parameter diperoleh berupa array dari function filterByRarity
     let randomNumber =  Math.floor(Math.random() * group.length); //diperoleh number antara 0 sampai index terakhir array group
@@ -140,15 +136,15 @@ function groupChoice(group) {//parameter diperoleh berupa array dari function fi
 }
 
 //function gacha memberikan secara acak item dengan harga yang murah
-function gacha() {
+function gacha() { 
     const gachaPrice = 10_000;
-    if (userWallet < gachaPrice) {//biar end function excetion jika user wallet kurang
+    if (userWallet <= gachaPrice) {//biar end function excetion jika user wallet kurang
         return;
     }
 
-    const RANGE = 100;
+    let RANGE = 100;
     let randomNumber = Math.ceil(Math.random() * RANGE);//memperoleh angka antara 1 sampai RANGE
-    let rarity = 'legend';
+    let rarity = 'legend'; //flagging
     if (randomNumber < 40) {//klo mau ditampilkan rarity, variable bisa dibuat global
         rarity = 'poor';
     } else if (randomNumber < 75) {
@@ -165,8 +161,40 @@ function gacha() {
     db_skin[prize].stock--; //sepertinya harus dibuat function update
 }
 
+//function signIn digunakan sebagai filter data
+function signIn() {
+    let inputPassword = document.getElementById('password');//box password
+    userInput = document.getElementById('input'); //box id
+    if (userInput in db_ID && inputPassword === db_ID[userInput].password) {
+        location.href = 'halaman baru html';//<file html>
+    } else {
+        alert("Login gagal");//untuk sementara yang gampang
+    }
+}
+
+//function signUp digunakan untuk menambah data baru di database
+function signUp() {
+    let newID = document.getElementById('nama pendaftar');//jika sempat kasih fitur filter password yang diizinkan (opsional)
+    let newPass = document.getElementById('password user baru');
+    db_ID[newID].password = {password: newPass, wallet: 0};//karena pengguna baru wallet kosong, mungkin kasih halaman fitur topup 
+}
 
 //test case
+let skin2 = 'skin2', skin6 = 'skin6'
+console.log(userWallet, collectedMoney, userChart, '<<1');
+getMoney(100_000);
+console.log(userWallet, collectedMoney, userChart,'<<2');
+calculation(skin2);
+console.log(userWallet, collectedMoney, userChart,'<<3');
+cancel();
+console.log(userWallet, collectedMoney, userChart,'<<4');
+gacha();
+gacha();
+console.log(userWallet, collectedMoney, userChart,'<<5');
+cancel();
+console.log(userWallet, collectedMoney, userChart,'<<6');
+reset();
+console.log(userWallet, collectedMoney, userChart,'<<7');
 // console.log(userWallet, collectedMoney, userChart, db_skin[itemClicked1].stock, db_skin[itemClicked2].stock, '<<<1');
 // getMoney(150_000);
 // console.log(userWallet, collectedMoney, userChart, db_skin[itemClicked1].stock, db_skin[itemClicked2].stock, '<<<2');
@@ -174,10 +202,10 @@ function gacha() {
 // console.log(userWallet, collectedMoney, userChart, db_skin[itemClicked1].stock, db_skin[itemClicked2].stock, '<<<3');
 // calculation(itemClicked2);
 // console.log(userWallet, collectedMoney, userChart, db_skin[itemClicked1].stock, db_skin[itemClicked2].stock, '<<<4');
-// gacha();
-// console.log(userWallet, collectedMoney, userChart, db_skin[itemClicked1].stock, db_skin[itemClicked2].stock, '<<<5');
-// reset();
-// console.log(userWallet, collectedMoney, userChart, db_skin[itemClicked1].stock, db_skin[itemClicked2].stock, '<<<6');
+// // gacha();
+// // console.log(userWallet, collectedMoney, userChart, db_skin[itemClicked1].stock, db_skin[itemClicked2].stock, '<<<5');
+// // reset();
+// // console.log(userWallet, collectedMoney, userChart, db_skin[itemClicked1].stock, db_skin[itemClicked2].stock, '<<<6');
 // checkOut();
 // console.log(userWallet, collectedMoney, userChart, db_skin[itemClicked1].stock, db_skin[itemClicked2].stock, '<<<7');
 
